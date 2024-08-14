f82085a53b530e2e2e3df21fecbf98a20920f508
01Laurent <kiirulaurent@gmail.com>
2024-08-14 20:26:19 +0300

    Python

--- Week1/practice.py
+++ Week1/practice.py
@@ -8,7 +8,9 @@ print(hello)
 
 # boolean variables
 IsPythonFun = True
+IsPythonBoring = False
 print(f"Is python fun? {IsPythonFun}")
+print(f"Is python boring? {IsPythonBoring}")
 
 # control flow: IF, ELIF, ELSE
 temperature = 15
@@ -37,3 +39,54 @@ def greet(name):
 #apply the function
 print(greet("Laurent"))
 print(greet("Annah"))
+
+
+# ELSE IF ELIF
+feast = 100
+if feast > 90:
+    print("The meal is delicious.")
+elif feast >= 50:
+    print("The meal is Yummy.")
+else:
+    print("The meal is nice.")
+
+# LOOPS
+for i in range(7):
+    print(f"The door bell rings twice {i}")
+
+countDown = 4
+while countDown > 0:
+    print(f"The countdown is {countDown}")
+    countDown -= 1
+print("The count down is over!!!")
+
+def hello(someone):
+    return f"Good Evening, {someone}!"
+#application of the function
+print(hello("Laurent"))
+print(hello("Yazmin"))
+
+#lists and dictionaries
+# A list can be ordered and indexed.
+# Dictionaries use key value pairs.
+fruitsList = ["apple", "Mango", "Melon"]
+fruitsTuple = ("cherry", "plums", "avocado")
+print(fruitsList)
+print(fruitsTuple)
+
+#Dictionaries
+contacts = {
+    "Yazmin": "9876540",
+    "Laurent": "987654",
+    "Bob": "345678"
+}
+print(f"This contact belongs to Yazmin: {contacts["Yazmin"]}")
+
+#modules
+import math
+result = math.sqrt(64)
+print(f"The square root of 64 is : {result}")
+
+#Custom
+import practice
+print(practice.hello("Yazmin"))
