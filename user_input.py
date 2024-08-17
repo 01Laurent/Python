6f4c7c4d9c1ec5bb5fb19bb12d460e57861bd927
01Laurent <kiirulaurent@gmail.com>
2024-08-17 11:05:53 +0300

    assignment one

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Assignment one
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+print(f"Hello, my name is {name}, I am {age} years old and i'm from {location}. Nice to meet you!")
